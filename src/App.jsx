5ba023502b1e8d1b4a83fa1dc91be4b05ace4080
SURYANSH <suryanshpatwal@gmail.com>
2024-11-24 14:58:16 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Modules from './components/Modules';
 import ImagesSection from './components/ImagesSection';
 import Footer from './components/Footer';
 import ActivityZone from './components/ActivityZone';
+import Over from './components/Over';
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header />
-      <ActivityZone /> {/* Add the Front component here */}
-      <main className="flex flex-col flex-grow justify-center items-center">
+      <Over/>
+      
+      {/* <Header />
+      <ActivityZone /> 
+      <main className="flex flex-col items-center justify-center flex-grow">
         <Modules />
         <ImagesSection />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
---
+++ src/components/Over.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Over = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="mb-4 text-4xl font-bold text-primary">
+        Thank You for Participating in the November Session of Activity Zone!
+      </h1>
+      <p className="text-lg text-primary">
+        We hope you had an enriching and exciting time. Stay tuned, and we'll see you in the next session soon! Keep an eye out for updates.
+      </p>
+    </div>
+  );
+};
+
+export default Over;
