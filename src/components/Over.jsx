65b8d2f199b156041d0ea4307c4633440b792436
SURYANSH <suryanshpatwal@gmail.com>
2024-11-24 15:06:08 +0530

    changes done

--- src/components/Over.jsx
+++ src/components/Over.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
 const Over = () => {
-  const [isClicked, setIsClicked] = useState(false);
+//   const [isClicked, setIsClicked] = useState(false);
 
-  const handleClick = () => {
-    setIsClicked(true);
-    setTimeout(() => {
-      setIsClicked(false);
-      window.open("https://linktr.ee/mlsa_miet", "_blank");
-    }, 300); // Animation duration before opening the link
-  };
+//   const handleClick = () => {
+//     setIsClicked(true);
+//     setTimeout(() => {
+//       setIsClicked(false);
+//       window.open("https://linktr.ee/mlsa_miet", "_blank");
+//     }, 300); // Animation duration before opening the link
+//   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
@@ -19,14 +19,14 @@ const Over = () => {
       <p className="mb-8 text-lg text-primary">
         We hope you had an enriching and exciting time. Stay tuned, and we'll see you in the next session soon! Keep an eye out for updates.
       </p>
-      <button
+      {/* <button
         onClick={handleClick}
         className={`px-6 py-3 font-medium text-white transition rounded-lg shadow bg-primary hover:bg-primary-dark ${
           isClicked ? "scale-95" : "scale-100"
         }`}
       >
         Visit Socials
-      </button>
+      </button> */}
     </div>
   );
 };
